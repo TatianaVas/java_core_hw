45f704e9a9c8abedd16cba73f4e03cac70945b9f
TatianaVas <tanya_zuzu@mail.ru>
2021-11-05 01:03:53 +0300

    complete classwork

--- src/main/java/ClassWork2.java
+++ src/main/java/ClassWork2.java
@@ -19,20 +19,62 @@ public class ClassWork2 {
         //    counter++;
        // if (counter % 3 == 0){
         //    continue;
-        for (int a = 0, b= 10, c =100; a < 10 && b > 0 && c >10; a++, b--, c-=10){
-            System.out.printf(" a = %d, b = %d, c = %d\n", a, b, c);
+       // for (int a = 0, b= 10, c =100; a < 10 && b > 0 && c >10; a++, b--, c-=10){
+           // System.out.printf(" a = %d, b = %d, c = %d\n", a, b, c);
+       // }
+
+      //  for (int i = 0; i < 15; i++){
+         //   System.out.println(i);
+
+
+      //  if (i == 5) {
+      //      break;
+      //  }
+      //  int counter = 1;
+      //  for( ; ;) {
+      //      System.out.println(counter++);
+        //}
+
+        String[] strings = {
+                "March",
+                "April",
+                "May",
+                "June"
+        };
+
+        for (String s : strings){
+              System.out.println(s);
         }
 
-        for (int i = 0; i < 15; i++){
-            System.out.println(i);
 
+        int[] arr;
+        arr = new int[5];
+        arr[0] =10;
+        arr[1] =20;
+        arr[2] =30;
+        arr[3] =110;
+        arr[4] =5;
 
-        if (i == 5) {
-            break;
+        arr[1] = (arr[1] + arr[3]) / arr[2];
+        System.out.println(arr[1]);
+
+        int[] arr1 = {1, 2, 3, 5, 6, 7, 8, 9, 9, 4};
+
+        for (int i = 0; i < arr1.length; i++){
+            arr1[i] *=2;
         }
-        int counter = 1;
-        for( ; ;) {
-            System.out.println(counter++);
+
+        for (int i = 0; i < arr1.length; i++){
+            System.out.print(arr1[i] + " ");
+        }
+
+
+        for (int t = 0; t < 10; t++) {
+            System.out.printf(" Exercise #%d: ", t);
+            for (int j = 1; j < 11; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
         }
 
         //    System.out.println(counter++);
@@ -48,20 +90,20 @@ public class ClassWork2 {
     //public static void printArgumentValueIntoConsole(String word){
        // System.out.printf(" Your argument was: %s\n", word);
 
-    }
 
-    public static double calculateSomeExpression(double firstValue, double secondValue){
-        double result = firstValue / secondValue;
-        System.out.println( " Calculating...");
+
+    //public static double calculateSomeExpression(double firstValue, double secondValue){
+       // double result = firstValue / secondValue;
+       // System.out.println( " Calculating...");
     //    System.out.println("first value: " + firstValue + " ; second value: "
          //       + secondValue + " ; result is: " + result);
       //  System.out.printf("First value: %f; second value: %f; result is: %f\n",
             //    firstValue, secondValue, result);
-        String s = String.format("First value: %f; second value: %f; result is: %f\n",
+      /*  String s = String.format("First value: %f; second value: %f; result is: %f\n",
           firstValue, secondValue, result);
         System.out.print(s);
         return result;
-    }
+
     public static void vendingWithIfs(){
         int choice = -10;
 
@@ -98,9 +140,10 @@ public class ClassWork2 {
                 System.out.println(" Give Snickers");
                 break;
             default:
-                System.out.println(" Error. Try again");
+                System.out.println(" Error. Try again"); */
 
         }
-    }
 
-}
+
+
+
