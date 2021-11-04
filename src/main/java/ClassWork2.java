c7d78cefcb35fcd651930065d6ab65ebfc4785b0
TatianaVas <tanya_zuzu@mail.ru>
2021-11-04 23:45:51 +0300

    complete classwork

---
+++ src/main/java/ClassWork2.java
@@ -0,0 +1,106 @@
+public class ClassWork2 {
+    public static void main(String[] args){
+      //  printSomethingIntoConsole();
+      //  String word = "Hello";
+      //  printArgumentValueIntoConsole(word);
+
+       // double someNumber = calculateSomeExpression(.24, 5.25);
+
+       // vendingWithIfs();
+       // vendingWithSwitch();
+
+      //  int counter = 1;
+
+      //  do {
+       //     System.out.println(counter++);
+      //  } while (counter < 0);
+
+        //while (counter < 21) {
+        //    counter++;
+       // if (counter % 3 == 0){
+        //    continue;
+        for (int a = 0, b= 10, c =100; a < 10 && b > 0 && c >10; a++, b--, c-=10){
+            System.out.printf(" a = %d, b = %d, c = %d\n", a, b, c);
+        }
+
+        for (int i = 0; i < 15; i++){
+            System.out.println(i);
+
+
+        if (i == 5) {
+            break;
+        }
+        int counter = 1;
+        for( ; ;) {
+            System.out.println(counter++);
+        }
+
+        //    System.out.println(counter++);
+      //  if (counter == 10){
+          //  break;
+        }
+
+
+    // public static void printSomethingIntoConsole(){
+        //System.out.println(" Something");
+
+
+    //public static void printArgumentValueIntoConsole(String word){
+       // System.out.printf(" Your argument was: %s\n", word);
+
+    }
+
+    public static double calculateSomeExpression(double firstValue, double secondValue){
+        double result = firstValue / secondValue;
+        System.out.println( " Calculating...");
+    //    System.out.println("first value: " + firstValue + " ; second value: "
+         //       + secondValue + " ; result is: " + result);
+      //  System.out.printf("First value: %f; second value: %f; result is: %f\n",
+            //    firstValue, secondValue, result);
+        String s = String.format("First value: %f; second value: %f; result is: %f\n",
+          firstValue, secondValue, result);
+        System.out.print(s);
+        return result;
+    }
+    public static void vendingWithIfs(){
+        int choice = -10;
+
+        if(choice== 1) {
+            System.out.println("Give Lays");
+        } else if (choice == 2) {
+            System.out.println(" Give Cheetos");
+        } else if (choice == 3) {
+        System.out.println(" Give Coca Cola");
+         } else if (choice == 4) {
+        System.out.println(" Give Pepsi");
+         } else if (choice == 5) {
+        System.out.println(" Give Snickers");
+        } else {
+            System.out.println(" Error. Try again");
+        }
+    }
+    public static void vendingWithSwitch(){
+        int choice = 2;
+        switch (choice) {
+            case 1:
+                System.out.println("Give Lays");
+                break;
+            case 2:
+                System.out.println(" Give Cheetos");
+                break;
+            case 3:
+                System.out.println(" Give Coca Cola");
+                break;
+            case 4:
+                System.out.println(" Give Pepsi");
+                break;
+            case 5:
+                System.out.println(" Give Snickers");
+                break;
+            default:
+                System.out.println(" Error. Try again");
+
+        }
+    }
+
+}
