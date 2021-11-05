bf0851eb6fb8f30c8d5f402d923e363211109ab3
TatianaVas <tanya_zuzu@mail.ru>
2021-11-05 20:06:03 +0300

    hw2 is done

--- src/main/java/HomeWork2.java
+++ src/main/java/HomeWork2.java
@@ -1,13 +1,41 @@
 public class HomeWork2 {
     public static void main(String[] args){
     checkTwoNumbers();
-    }
- public static void checkTwoNumbers(){
+    checkPositiveNumber();
+    checkNegativeNumber();
+    printSomething();
+        }
+
+
+
+    public static void checkTwoNumbers(){
         int number1 = 10;
         int number2 = 2;
         int sum = number1 + number2;
      System.out.println( " True");
  }
+ public static void checkPositiveNumber(){
+        int number = 16;
+        if (number>=0){
+            System.out.println(" Положительное число");
+        } else if (number<0){
+            System.out.println(" Отрицательное число");
+        }
+ }
+ public static void checkNegativeNumber(){
+        int numb = -150;
+        if (numb < 0){
+            System.out.println("True");
+        } else System.out.println("False");
+ }
+
+    public static void printSomething() {
+    String s = " Java is cool";
+        for (int n = 0; n < 9; n++) System.out.println(s);
+ }
 
 
-}
+  }
+
+
+
